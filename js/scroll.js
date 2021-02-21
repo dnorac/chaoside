e985c88731509d490833a65c6f0a0cb38b58ec86
Daniel Nora <daniel_nora@hotmail.com>
2021-02-21 17:09:41 -0300

    Many fixes

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 const scrollCursor = document.querySelector('#cursor');
 
 const handleScroll = (event) => {
-    if (window.scrollY > 700) {
+    if (window.scrollY > window.innerHeight - 50) {
         header.classList.add('dark');
     } else {
         header.classList.remove('dark');
