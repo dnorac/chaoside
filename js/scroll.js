82e6384d33c1bdc6484074b99d765def5314c6aa
Daniel Nora <daniel_nora@hotmail.com>
2021-03-10 18:31:31 -0300

    Modified pgdown/pgup keys

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 const scrollCursor = document.querySelector('#cursor');
 
-const handleScroll = (event) => {
+const handleScroll = () => {
     if (window.scrollY > window.innerHeight - 50) {
         header.classList.add('dark');
     } else {
@@ -9,3 +9,19 @@ const handleScroll = (event) => {
     }
 }
 document.addEventListener('scroll', handleScroll);
+
+document.addEventListener('keydown', (e) => {
+    if (!(e.key === 'PageDown' || e.key === 'PageUp')) return;
+    const locations = ['', '#vision', '#contact']
+    e.preventDefault();
+    let currentLocation = locations.findIndex(l => l === location.hash);
+    const nextLocation = currentLocation => {
+        if (e.key === 'PageDown') {
+            return currentLocation === locations.length - 1 ? 0 : currentLocation + 1
+        } else {
+            return currentLocation === 0 ? locations.length - 1 : currentLocation - 1
+        }
+    }
+    console.log(nextLocation(currentLocation))
+    location.hash = locations[nextLocation(currentLocation)]
+})
